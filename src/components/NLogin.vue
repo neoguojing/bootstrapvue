033de3c80f34a1957628d94c33bb07556fbeb4b4
neoguojing <guojing_neo@163.com>
2020-03-04 10:50:16 +0800

    re design home page

---
+++ src/components/NLogin.vue
@@ -0,0 +1,62 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue,Emit,Inject,Provide,Watch,Model } from 'vue-property-decorator';
+import { BModal,BButton  } from 'bootstrap-vue';
+
+@Component({
+    components: {
+        BModal,
+        BButton
+    }
+})
+export default class NLogin extends Vue {
+    @Prop(String) btnSize?: string;
+    @Prop({default: "primary"}) btnBackgroud?: string;
+    @Prop(String) btnName?: string;
+    @Prop(String) modalTitle?: string;
+    
+    /////////////////////////////////////////////
+    handleShow(evt: object){
+        this.showModal(evt)
+    }
+    @Emit("show")
+    showModal(evt: object){
+        return evt 
+    }
+
+    handleHidden(evt: object){
+        this.hiddenModal(evt)
+    }
+    @Emit("hidden")
+    hiddenModal(evt: object){
+        return evt
+    }
+
+    handleOk(evt: object){
+        this.okModal(evt)
+    }
+    @Emit("ok")
+    okModal(evt: object){
+        return evt
+    }
+
+    handleCancel(evt: object){
+        this.cancelModal(evt)
+    }
+    @Emit("cancel")
+    cancelModal(evt: object){
+        return evt
+    }
+
+    handleClose(evt: object){
+        this.closeModal(evt)
+    }
+    @Emit("close")
+    closeModal(evt: object){
+        return evt
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,60 +10,96 @@
           :imgList="imgList"
           @curSlide="getCurrentSlide"
         ></NCarousel>
-    </BCol>
+      </BCol>
     </BRow>
 
     <BRow>
-      <BCol>
-        <NModal 
-          btnName="my-table"
-          modalTitle="对话框"
-          @show="handleModalEnvet"
-          @hidden="handleModalEnvet"
-          @close="handleModalEnvet"
-          @cancel="handleModalEnvet"
-          @ok="handleModalEnvet"
-          btnSize="lg"
-        ></NModal>
-    </BCol>
+      <BCol cols="9">
+        <b-card no-body>
+          <b-tabs card fill>
+            <b-tab no-body title="NPageTable">
+              <BRow>
+                <BCol>
+                  <NPageTable
+                    :items="items" 
+                    :perPage="perPage" 
+                    id="my-page-table" 
+                  ></NPageTable>
+                </BCol>
+              </BRow>
+            </b-tab>
+
+            <b-tab no-body title="Page+Table">
+              <BRow>
+                <BCol>
+                  <NTable 
+                    id="my-table"
+                    :items="items"
+                    :perPage="perPage"
+                    :currentPage="currentPage"
+                  ></NTable>
+                </BCol>
+              </BRow>
+              <BRow>
+                <BCol>
+                  <NPagination
+                    :rows="rows" 
+                    :perPage="perPage" 
+                    target="my-table" 
+                    v-on:pageChanged="setCurrentPage"
+                  ></NPagination>
+                </BCol>
+              </BRow>
+            </b-tab>
+
+            <b-tab title="Modal">
+              <BRow>
+                <BCol>
+                  <b-card
+                    overlay
+                    title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    class="mb-2"
+                  >
+                    <b-card-text>
+                      Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </b-card-text>
+
+                    <NModal 
+                      btnName="my-table"
+                      modalTitle="对话框"
+                      @show="handleModalEnvet"
+                      @hidden="handleModalEnvet"
+                      @close="handleModalEnvet"
+                      @cancel="handleModalEnvet"
+                      @ok="handleModalEnvet"
+                      btnSize="lg"
+                    ></NModal>
+
+                  </b-card>
+                </BCol>
+              </BRow>
+            </b-tab>
+
+          </b-tabs>
+        </b-card>
+      </BCol>
+
+      <BCol cols="3">
+        <b-calendar locale="zh">
+        </b-calendar>
+      </BCol>
     </BRow>
 
-    <BRow>
-      <BCol>
-        <NTable 
-          id="my-table"
-          :items="items"
-          :perPage="perPage"
-          :currentPage="currentPage"
-        ></NTable>
-    </BCol>
-    </BRow>
-    <BRow>
-      <BCol>
-        <NPagination
-          :rows="rows" 
-          :perPage="perPage" 
-          target="my-table" 
-          v-on:pageChanged="setCurrentPage"
-        ></NPagination>
-    </BCol>
-    </BRow>
-
-    <BRow>
-      <BCol>
-        <NPageTable
-          :items="items" 
-          :perPage="perPage" 
-          id="my-page-table" 
-        ></NPageTable>
-    </BCol>
-    </BRow>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { BRow,BCol } from 'bootstrap-vue';
+import { BRow,BCol,BCard,BTab,BTabs,BCardText,BCalendar  } from 'bootstrap-vue';
 import NPagination from '@/components/NPagination.vue'
 import NTable from '@/components/NTable.vue'
 import NCarousel from '@/components/NCarousel.vue'
@@ -77,7 +113,12 @@ export default { //导出模块
     NTable,
     NCarousel,
     NPageTable,
-    NModal
+    NModal,
+    BTab,
+    BCard,
+    BTabs,
+    BCardText,
+    BCalendar
   },
   data() {
       return {
