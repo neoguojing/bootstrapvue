ed6f597c47ffb92d092b7d559b292c5bedcc97ea
guoerjun <guoerjun@sensetime.com>
2021-11-30 16:30:21 +0800

    init

--- src/protocol/ForgetPassword.ts
+++
@@ -1,7 +0,0 @@
-export default class ForgetPasswordData {
-    Email: string;
-
-    constructor() {
-        this.Email = "";
-    }
-}
--- src/protocol/ResetPassword.ts
+++
@@ -1,9 +0,0 @@
-export default class ResetPasswordData {
-    OldPassword: string;
-    Password: string;
-
-    constructor() {
-        this.OldPassword = "";
-        this.Password = "";
-    }
-}
