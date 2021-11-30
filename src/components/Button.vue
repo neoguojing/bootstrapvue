a90779a647aae237cdfe5ff37c87df707fb3b4e1
guoerjun <guoerjun@sensetime.com>
2021-11-30 17:48:52 +0800

    add Button cop

---
+++ src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+    <button type="button" class="btn" :class="classObj" :disabled="disabled">{{ text }}</button>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+export default{
+    name:"Button",
+    props:{
+        text: {
+            type :String,
+            default: "Button"
+        },
+        size:{
+            type :String,
+            default: "lg"
+        },
+        color:{
+            type :String,
+            default: "primary"
+        },
+        disabled:{
+            type :Boolean,
+            default: false
+        }
+
+    },
+    computed:{
+        classObj() {
+           return [
+               'btn-'+this.size,
+               'btn-'+this.color
+           ]
+        }
+    }
+
+}
+</script>
+<style scoped lang="scss">
+    
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import Button from '@/components/Button.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Button
   }
 }
 </script>
