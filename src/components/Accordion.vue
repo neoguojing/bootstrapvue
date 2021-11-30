99ff6e898afcb20f85abb4a883bf8ed8a7fe72a6
guoerjun <guoerjun@sensetime.com>
2021-11-30 19:41:02 +0800

    add accordion

---
+++ src/components/Accordion.vue
@@ -0,0 +1,72 @@
+<template>
+<div class="accordion" :id="id">
+  <div v-for="(item,i) in list" :key="item.headName" class="accordion-item">
+    <h2 class="accordion-header" :id="item.headName">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" 
+        :data-bs-target="selectId(item.bodyName)" :aria-expanded="i==0" :aria-controls="item.bodyName">
+        {{ item.title }}
+      </button>
+    </h2>
+    <div :id="item.bodyName" class="accordion-collapse collapse" :class="isShow(i)" :aria-labelledby="item.headName" 
+        :data-bs-parent="selectId(id)">
+      <div class="accordion-body">
+           {{ item.content }}
+     </div>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+export default{
+    name:"Accordion",
+    props:{
+        id:{
+            type: String,
+            default:"accordionExample"
+        },
+        list:{
+           type: Array,
+           default:function(){
+               return [
+               {
+                   headName:"headingOne",
+                   title:"Accordion Item #1",
+                   bodyName:"collapseOne",
+                   content:"Accordion Item #1",
+               },
+               {
+                   headName:"headingTwo",
+                   title:"Accordion Item #2",
+                   bodyName:"collapseTwo",
+                   content:"Accordion Item #2",
+               },
+               {
+                   headName:"headingThree",
+                   title:"Accordion Item #3",
+                   bodyName:"collapseThree",
+                   content:"Accordion Item #3",
+               }
+            ]
+           }
+        }
+    },
+    computed:{
+        selectId(){
+            return function(name) {
+                return "#"+name
+            }
+        },
+        
+    },
+    
+    methods:{
+        isShow(idx){
+            return {
+                show:idx==0
+            }
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Button/>
+    <Button text="my button" size="mid" color="danger"/>
+    <Accordion/>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import Button from '@/components/Button.vue'
+import Accordion from '@/components/Accordion.vue'
 export default {
   name: 'Home',
   components: {
     HelloWorld,
-    Button
+    Button,
+    Accordion
   }
 }
 </script>
