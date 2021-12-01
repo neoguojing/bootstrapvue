fb0c6deb259d0cff2684fa64b158c68fa07c7424
guojingneo <guojing_neo@163.com>
2021-12-01 22:34:39 +0800

    up

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,50 @@
 <template>
-  <form class="row g-10 needs-validation" novalidate>
-  <div class="row g-5">
+<form class="row gy-5 needs-validation" novalidate>
+  <div class="row gy-5">
     <div class="col"></div>
-    <div class="input-group has-validation col">
-      <input type="text" class="form-control" id="username" aria-describedby="inputGroupPrepend" required>
+    <div class="col">
+      <div class="form-floating has-validation">
+        <input type="text" class="form-control" id="username" aria-describedby="inputGroupPrepend" required>
+        <label for="username">Username</label>
+      </div>
       <div class="invalid-feedback">
-        Please choose a username.
+          Please choose a username.
       </div>
     </div>
     <div class="col"></div>
   </div>
-  <div class="row g-3">
+  <div class="row gy-2">
     <div class="col"></div>
-    <div class="input-group has-validation col">
-      <input type="passward" class="form-control" id="passward" required>
+    <div class="col">
+      <div class="has-validation form-floating">
+        <input type="password" class="form-control" id="password" required>
+        <label for="password">Password</label>
+      </div>
       <div class="invalid-feedback">
-        Please provide passward.
+        Please provide a valid Password.
       </div>
     </div>
     <div class="col"></div>
   </div>
-  <div class="row g-3 justify-content-md-center ">
+  <div class="row gx-2">
+    <div class="col-4"></div>
     <div class="form-check col-2">
       <input class="form-check-input" type="checkbox" value="" id="remember" required>
       <label class="form-check-label" for="remember">
-       Remember passward.
+        Remember
       </label>
-
+      <div class="invalid-feedback">
+        You must agree before submitting.
+      </div>
     </div>
+    <div class="col-6"></div>
   </div>
-  <div class="row g-1 justify-content-md-center">
-    <button class="btn btn-primary col-1" type="submit">Login</button>
+  <div class="row gy-1">
+    <div class="col-5"></div>
+    <div class="col-2">
+    <button class="btn btn-primary" type="submit">Login</button>
+    </div>
+    <div class="col-5"></div>
   </div>
 </form>
 </template>
