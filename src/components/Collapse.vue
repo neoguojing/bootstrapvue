dc323363920187bb75b922bb58c8f6e70f2f07d8
guoerjun <guoerjun@sensetime.com>
2021-12-01 15:21:31 +0800

    add Collapse

---
+++ src/components/Collapse.vue
@@ -0,0 +1,56 @@
+<template>
+    <button class="btn" :class="classObj" type="button" data-bs-toggle="collapse" :data-bs-target="selectId(id)" :aria-expanded="expanded" :aria-controls="id">
+        {{ text }}
+    </button>
+    <div class="collapse" :id="id">
+    <div class="card card-body">
+        <slot></slot>
+    </div>
+    </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+export default{
+    name:"Collapse",
+    props:{
+        id:{
+            type: String,
+            default:"collapseExample"
+        },
+        text: {
+            type :String,
+            default: "Button with data-bs-target"
+        },
+        size:{
+            type :String,
+            default: "lg"
+        },
+        color:{
+            type :String,
+            default: "success"
+        },
+        expanded:{
+            type :Boolean,
+            default: false
+        }
+
+    },
+    computed:{
+        classObj() {
+           return [
+               'btn-'+this.size,
+               'btn-'+this.color
+           ]
+        }
+    },
+    methods:{
+        selectId(name){
+            return "#"+name
+        },
+    }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,9 @@
     <Accordion/>
     <Card/>
     <Carousel/>
+    <Collapse>
+    Collapse
+    </Collapse>
   </div>
 </template>
 
@@ -16,7 +19,7 @@ import Button from '@/components/Button.vue'
 import Accordion from '@/components/Accordion.vue'
 import Card from '@/components/Card.vue'
 import Carousel from '@/components/Carousel.vue'
-
+import Collapse from '@/components/Collapse.vue'
 export default {
   name: 'Home',
   components: {
@@ -24,7 +27,8 @@ export default {
     Button,
     Accordion,
     Card,
-    Carousel
+    Carousel,
+    Collapse
   }
 }
 
