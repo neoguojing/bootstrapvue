1a50c9f9d87a0f61f20d16d97a08765f146c5be8
guoerjun <guoerjun@sensetime.com>
2021-12-02 19:38:30 +0800

    up register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,67 +1,110 @@
 <template>
   <div  class="mx-auto" style="width:50%;">
-     <form class="row g-3  needs-validation" novalidate>
-      <div class="col-md-4">
-        <label for="validationCustom01" class="form-label">First name</label>
-        <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
-        <div class="valid-feedback">
-          Looks good!
+     <form class="row gy-5 p-5 needs-validation" :class="validateClass" novalidate>
+      <div class="col-md-4 form-floating">
+        <input type="text" class="form-control" id="firstName" v-model="firstName" placeholder="First name"  required>
+        <label for="firstName" class="form-label">First name</label>
+        <div class="invalid-feedback">
+           Please provide a valid first name.
+        </div>
+      </div>
+      <div class="col-md-4 form-floating">
+        <input type="text" class="form-control" id="lastName" v-model="lastName" placeholder="Last name"  required>
+        <label for="lastName" class="form-label">Last name</label>
+        <div class="invalid-feedback">
+          Please provide a valid last name.
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" v-model="gender" name="gender" id="1" value="Male">
+          <label class="form-check-label" for="man">
+            Man
+          </label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" v-model="gender" name="gender" id="woman" value="0" checked>
+          <label class="form-check-label" for="woman">
+            Woman
+          </label>
+        </div>
+      </div>
+
+      <div class="col-md-6 form-floating">
+        <input type="email" class="form-control" v-model="email" id="email" placeholder="Email" required>
+        <label for="email" class="form-label">Email</label>
+        <div class="invalid-feedback">
+          Please provide a valid email.
         </div>
       </div>
       <div class="col-md-4">
-        <label for="validationCustom02" class="form-label">Last name</label>
-        <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
-        <div class="valid-feedback">
-          Looks good!
-        </div>
-      </div>
-      <div class="col-md-4">
-        <label for="validationCustomUsername" class="form-label">Username</label>
-        <div class="input-group has-validation">
-          <span class="input-group-text" id="inputGroupPrepend">@</span>
-          <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+        <div class="has-validation form-floating">
+          <input type="text" class="form-control" id="userName" v-model="userName" placeholder="Username" aria-describedby="inputGroupPrepend" required>
+          <label for="userName" class="form-label">Username</label>
           <div class="invalid-feedback">
             Please choose a username.
           </div>
         </div>
       </div>
-      <div class="col-md-6">
-        <label for="validationCustom03" class="form-label">City</label>
-        <input type="text" class="form-control" id="validationCustom03" required>
+      <div class="col-md-5 form-floating">
+        <input type="text" class="form-control" id="city" v-model="city" placeholder="City" required>
+        <label for="city" class="form-label">City</label>
         <div class="invalid-feedback">
           Please provide a valid city.
         </div>
       </div>
-      <div class="col-md-3">
-        <label for="validationCustom04" class="form-label">State</label>
-        <select class="form-select" id="validationCustom04" required>
-          <option selected disabled value="">Choose...</option>
-          <option>...</option>
-        </select>
+      <div class="col-md-5 form-floating">
+        <input type="text" class="form-control" v-model="state" id="state" placeholder="State" required>
+        <label for="state" class="form-label">State</label>
         <div class="invalid-feedback">
-          Please select a valid state.
+          Please provide a valid state.
         </div>
       </div>
-      <div class="col-md-3">
-        <label for="validationCustom05" class="form-label">Zip</label>
-        <input type="text" class="form-control" id="validationCustom05" required>
+      <div class="col-md-5 form-floating">
+        <input type="password" class="form-control" v-model="password" id="password" placeholder="Password" required>
+        <label for="password" class="form-label">Password</label>
         <div class="invalid-feedback">
-          Please provide a valid zip.
+          Please provide a valid password.
         </div>
       </div>
-      <div class="col-12">
+      <div class="col-md-5 form-floating">
+        <input type="password" class="form-control" v-model="password1" id="password1" placeholder="Password again" required>
+        <label for="password1" class="form-label">Password again</label>
+        <div class="invalid-feedback">
+          Please confirm the password.
+        </div>
+      </div>
+      <div class="col-4">
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-          <label class="form-check-label" for="invalidCheck">
+          <input class="form-check-input" v-model="wasAgreeChecked" type="checkbox" value="true"  id="agree" required>
+          <a class="form-check-label" for="agree" data-bs-toggle="modal" data-bs-target="#terms">
             Agree to terms and conditions
-          </label>
+          </a>
           <div class="invalid-feedback">
             You must agree before submitting.
           </div>
+          <div class="modal fade" id="terms" tabindex="-1" aria-labelledby="#content" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="content">Terms and conditions</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  This is some placeholder content to show a vertically centered modal. We've added some extra copy here to show how vertically centering the modal works when combined with scrollable modals. We also use some repeated line breaks to quickly extend the height of the content, thereby triggering the scrolling. When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary" >Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="col-12">
-        <button class="btn btn-primary" type="submit">Submit</button>
+      <div class="col-9"></div>
+      <div class="col-3">
+        <button class="btn btn-primary" type="submit" @click.stop.prevent="onSubmit">Submit</button>
       </div>
     </form>
   </div>
@@ -69,9 +112,49 @@
 
 <script>
 // @ is an alias to /src
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import _ from 'lodash'
 export default {
+  
   name: 'Register',
   components: {
+  },
+   data(){
+    return {
+      wasValidated: false,
+      wasAgreeChecked: false,
+      firstName:"",
+      lastName:"",
+      userName:"",
+      gender:0,
+      city:"",
+      state:"",
+      email:"",
+      password:"",
+      password1:""
+    }
+  },
+  computed:{
+    validateClass(){
+      return {
+        'was-validated':this.wasValidated,
+      }
+    },
+  },
+  methods:{
+    onSubmit: _.debounce(function(){
+        this.wasValidated = true
+        if (!this.validate()){
+           return 
+        }
+        console.log("注册成功")
+         this.router.push("/login")
+      },1000),
+
+    validate(){
+      return true
+    }
   }
 }
 </script>
