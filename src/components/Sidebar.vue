fe70119ca1925ddab45cd928e6f0299475dd4e75
guojingneo <guojing_neo@163.com>
2021-12-04 21:05:52 +0800

    up

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -58,10 +58,10 @@
   </symbol>
 </svg>
   
-   <div id="sidebar" class="offcanvas offcanvas-start d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" tabindex="-1" style="width: 280px;">
+   <div :id="id" class="offcanvas offcanvas-start d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" tabindex="-1" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-4">Sidebar</span>
+      <span class="fs-4">{{ title }}</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
@@ -119,7 +119,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 export default {
-    name:"Sidebar"
+    name:"Sidebar",
+    props:{
+      id:{
+        type: String,
+        default:"sidebar"
+      },
+      title:{
+        type : String,
+        default:""
+      },
+    }
 }
 </script>
 
