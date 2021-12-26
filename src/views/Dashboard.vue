562f6527cc9b2a1acb5dd799e50a27158e8ac1eb
guojingneo <guojing_neo@163.com>
2021-12-26 22:27:51 +0800

    up

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,54 +3,61 @@
         <Sidebar id="dashboardSidebar" title="Dashboard" :offcanvasClass="[]">
             <ul class="nav nav-pills flex-column mb-auto ">
                 <li class="nav-item ">
-                    <a href="#" class="nav-link active d-flex justify-content-start" aria-current="page">
+                    <a href="#" class="nav-link active d-flex justify-content-start" 
+                    id="main-tab" data-bs-toggle="pill" data-bs-target="#main" role="tab" aria-controls="main" aria-selected="true">
                     <i class="bi bi-person-circle px-3" width="16" height="16"></i>
                     Main
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link text-dark  d-flex justify-content-start">
+                    <a href="#" class="nav-link text-dark  d-flex justify-content-start"  
+                    id="manager-tab" data-bs-toggle="pill" data-bs-target="#manager" role="tab" aria-controls="manager" aria-selected="false">
                     <i class="bi bi-gear px-3" width="16" height="16"></i>
                     Manager
                     </a>
                 </li>    
             </ul>
         </Sidebar>
-        <div class="col" >
-            <PageTable @pageChange="onPageChange" :numOfPerPage="rowsPerPage" :total="totalRows" >
-                <table class="table caption-top">
-                    <caption>List of resumes</caption>
-                    <thead>
-                        <tr>
-                            <th  scope="col"> Name </th>
-                            <th  scope="col"> Phone</th>
-                            <th  scope="col"> Email</th>
-                            <th  scope="col"> Experience(years)</th>
-                            <th  scope="col"> Summary</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr @click="onRowClick(item)" v-for="item in items" v-bind:key="item">
-                            <td > {{ item.Name }}</td>
-                            <td > {{ item.MobileNumber }}</td>
-                            <td > {{ item.Email }}</td>
-                            <td > {{ item.TotalExperience }}</td>
-                            <td > {{ item.Skills }}</td>
-                            <td > <div class="btn-group">
-                                <button class="btn btn-primary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Operation
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" onRowClick(item)>View</a></li>
-                                    <li><a class="dropdown-item" onEditClick(item)>Edit</a></li>
-                                    <li><a class="dropdown-item" onDelClick(item.id)>Delete</a></li>
-                                </ul>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </PageTable>
+        <div class="col tab-content" id="tabContent">
+            <div class="tab-pane fade show active" id="main" role="tabpanel" aria-labelledby="main-tab">
+                <PageTable @pageChange="onPageChange" :numOfPerPage="rowsPerPage" :total="totalRows" >
+                    <table class="table caption-top">
+                        <caption>List of resumes</caption>
+                        <thead>
+                            <tr>
+                                <th  scope="col"> Name </th>
+                                <th  scope="col"> Phone</th>
+                                <th  scope="col"> Email</th>
+                                <th  scope="col"> Experience(years)</th>
+                                <th  scope="col"> Summary</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr @click="onRowClick(item)" v-for="item in items" v-bind:key="item">
+                                <td > {{ item.Name }}</td>
+                                <td > {{ item.MobileNumber }}</td>
+                                <td > {{ item.Email }}</td>
+                                <td > {{ item.TotalExperience }}</td>
+                                <td > {{ item.Designation }}</td>
+                                <td > <div class="btn-group">
+                                    <button class="btn btn-primary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Operation
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" onRowClick(item)>View</a></li>
+                                        <li><a class="dropdown-item" onEditClick(item)>Edit</a></li>
+                                        <li><a class="dropdown-item" onDelClick(item.id)>Delete</a></li>
+                                    </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </PageTable>
+            </div>
+            <div class="tab-pane fade" id="manager" role="tabpanel" aria-labelledby="manager-tab">
+                hahah
+            </div>
         </div>
     </div>
 </template>
@@ -68,7 +75,7 @@ export default {
                 {name:'1',mobilenumber:'Mark',email:'Otto',totalexperience:'@mdo',skills:[]},
                 {name:'1',mobilenumber:'Mark',email:'Otto',totalexperience:'@mdo',skills:[]},
             ],
-            rowsPerPage: 1,
+            rowsPerPage: 10,
             totalRows : 0,
 
         }
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,21 +3,28 @@
         <Sidebar id="settingsSidebar" title="Settings" :offcanvasClass="[]">
             <ul class="nav nav-pills flex-column mb-auto ">
                 <li class="nav-item ">
-                    <a href="#" class="nav-link active d-flex justify-content-start" aria-current="page">
+                    <a href="#" class="nav-link active d-flex justify-content-start" 
+                     id="profile-tab" data-bs-toggle="pill" data-bs-target="#profile" role="tab" aria-controls="profile" aria-selected="true">
                     <i class="bi bi-person-circle px-3" width="16" height="16"></i>
                     Profile
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link text-dark  d-flex justify-content-start">
+                    <a href="#" class="nav-link text-dark  d-flex justify-content-start" 
+                     id="reset-tab" data-bs-toggle="pill" data-bs-target="#reset" role="tab" aria-controls="reset">
                     <i class="bi bi-gear px-3" width="16" height="16"></i>
                     Reset password
                     </a>
                 </li>    
             </ul>
         </Sidebar>
-        <div class="col" >
-            <ProfileBig/>
+        <div class="col tab-content" id="tabContent" >
+            <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                <ProfileBig/>
+            </div>
+            <div class="tab-pane fade" id="reset" role="tabpanel" aria-labelledby="reset-tab">
+                hahah
+            </div>
         </div>
     </div>
 </template>
